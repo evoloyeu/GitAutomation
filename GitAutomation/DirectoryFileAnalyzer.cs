6ff414cb67b2109638c3edeeb9a734567928a2c7
Rexl <rex.cheng.lei@gmail.com>
2020-05-16 01:38:48 -0700

    Add DirectoryFileAnalyzer class

---
+++ GitAutomation/DirectoryFileAnalyzer.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace GitAutomation
+{
+    public class DirectoryFileAnalyzer
+    {
+        public DirectoryFileAnalyzer()
+        {
+        }
+
+        // Get directory size: gigabytes
+        public static float GetDirectorySize(string fileName)
+        {
+            float size = 0;
+
+            if(Directory.Exists(fileName))
+            {
+                var dirInfo = new DirectoryInfo(fileName);
+                foreach (FileInfo fiInfo in dirInfo.GetFiles("*", SearchOption.AllDirectories))
+                {
+                    size += fiInfo.Length;
+                }
+
+                size = size / (1024f * 1024f * 1024f);
+            }
+
+            return size;
+        }
+
+        // Get file size in Megabytes
+        public static float GetFileSize(string fileName)
+        {
+            float size = 0;
+
+            if (File.Exists(fileName))
+            {
+                var fileInfo = new FileInfo(fileName);
+                size = fileInfo.Length / (1024 * 1024f);
+            }
+
+            return size;
+        }
+
+        // The hidden files are excluded and the files in hidden folders are excluded as well
+        public static List<FileInfo> GetFilesInDirectory(string directoryName)
+        {
+            if(Directory.Exists(directoryName))
+            {
+                var dirInfo = new DirectoryInfo(directoryName);
+                return dirInfo.GetFiles("*", SearchOption.AllDirectories).Where(f=>!f.Attributes.HasFlag(FileAttributes.Hidden) && !f.FullName.Contains(".git")).ToList();
+            }
+
+            return new List<FileInfo>();
+        }
+    }
+}
---
+++ GitAutomation/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+ 
+namespace GitAutomation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Console.WriteLine("Hello World!");
+            var currentDirectory = Directory.GetCurrentDirectory();
+            var dirInfo = new DirectoryInfo(@"/Volumes/Seagate Backup Plus Drive/Work/LiAoShow");
+            long size = 0;
+            foreach(FileInfo fiInfo in dirInfo.GetFiles("*", SearchOption.AllDirectories))
+            {
+                size += fiInfo.Length;
+            }
+
+            Console.WriteLine($"Directory Size: {size/(1024f*1024f*1024f)} GB");*/
+            var directory = @"/Volumes/Seagate Backup Plus Drive/Work/LiAoShow/李敖有话说/视频";
+            //Print directory size
+            var size = DirectoryFileAnalyzer.GetDirectorySize(directory);
+            Console.WriteLine($"{directory}: size: {size} GB");
+            
+
+            // Print File Size
+            foreach(var fileInfo in DirectoryFileAnalyzer.GetFilesInDirectory(directory))
+            {
+                Console.WriteLine($"size:{DirectoryFileAnalyzer.GetFileSize(fileInfo.FullName)} MB : {fileInfo.Name}");
+            }
+        }
+    }
+}
